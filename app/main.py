d1eead6471ba7e0beaa2aa879a395ec93e387fa9
Miloš Lipničan <lipncianmilos@gmail.com>
2025-11-19 19:07:55 +0100

    Revert "fix"
    This reverts commit 43105d0279f8076ffe32d41b51c90af39dde14d9.

--- app/main.py
+++ app/main.py
@@ -598,8 +598,7 @@ async def startup_event():
 
 if __name__ == "__main__":
     import uvicorn
-    port = int(os.environ.get("PORT", 8080))
-    uvicorn.run(app, host="0.0.0.0", port=port)
+    uvicorn.run(app, host="0.0.0.0", port=8000, reload=True)
 
 # New endpoint to get user statistics
 @app.get("/api/user/stats")
